f4f07a4184a7fa7f56968d281591658a43e23dbe
Tike <124289043+TarasKostr@users.noreply.github.com>
2026-04-30 19:46:08 +0300

    changes or smth

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import CarInfo from "./components/CarInfo";
-import { Navigation, Gauge } from "lucide-react";
+import { Navigation, Gauge, Battery } from "lucide-react";
 
 const App = () => {
   const [systems, setSystems] = useState({
-    autopilot: false,
+    autopilot: true,
     lights: true,
     climate: false,
   });
+
+  const [range, setRange] = useState(500);
+  const [speed, setSpeed] = useState(200);
+  const [battery, setBattery] = useState(51);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(systems.autopilot){
+        setSpeed(prev => Math.min(prev + 5, 300))
+        setRange(prev => Math.max(prev - 0.5, 0))
+        setBattery(prev => Math.max(prev - 0.1, 0))
+      }else{
+        setSpeed(prev => Math.max(prev - 8, 0))
+      }
+    }, 500)
+
+    return () => clearInterval(interval)
+  }, [systems.autopilot])
 
   const toggleSystems = (key) => {
     setSystems(prev => ({ ...prev, [key]: !prev[key] }));
@@ -18,11 +36,9 @@ const App = () => {
     <div className="bg-blue-100">
       <div className="min-h-screen bg-blue-200 text-violet-500 p-6 md:p-12 max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
-          <CarInfo label="Запас ходу" value={500} unit="км" icon={Navigation} className="text-amber-800!" />
-          <CarInfo label="Швидкість" value={200} unit="км" icon={Gauge} className="text-amber-800" />
-          <CarInfo label="Запас ходу" value={500} unit="км" icon={Navigation} className="text-amber-800!" />
-          <CarInfo label="Запас ходу" value={500} unit="км" icon={Navigation} className="text-amber-800!" />
-          <CarInfo label="Запас ходу" value={500} unit="км" icon={Navigation} className="text-amber-800!" />
+          <CarInfo label="Запас ходу" value={range.toFixed(0)} unit="км" icon={Navigation} className="text-white" />
+          <CarInfo label="Швидкість" value={speed} unit="км" icon={Gauge} className="text-white" />
+          <CarInfo label="Заряд" value={battery.toFixed(1)} unit="%" icon={Battery} className="text-white" />
         </div>
       </div>
     </div>
