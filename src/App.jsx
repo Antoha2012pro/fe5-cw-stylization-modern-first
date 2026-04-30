249a43bfa31b44e894074dde99723ffb73480a89
Andriy <andriy220kor@gmail.com>
2026-04-30 20:27:10 +0300

    SystemToggle

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CarInfo from "./components/CarInfo";
 import { Navigation, Gauge, Battery } from "lucide-react";
+import SystemToggle from "./components/SystemToggle";
 
 const App = () => {
   const [systems, setSystems] = useState({
@@ -16,11 +17,11 @@ const App = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if(systems.autopilot){
+      if (systems.autopilot) {
         setSpeed(prev => Math.min(prev + 5, 300))
         setRange(prev => Math.max(prev - 0.5, 0))
         setBattery(prev => Math.max(prev - 0.1, 0))
-      }else{
+      } else {
         setSpeed(prev => Math.max(prev - 8, 0))
       }
     }, 500)
@@ -37,8 +38,16 @@ const App = () => {
       <div className="min-h-screen bg-blue-200 text-violet-500 p-6 md:p-12 max-w-7xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
           <CarInfo label="Запас ходу" value={range.toFixed(0)} unit="км" icon={Navigation} className="text-white" />
-          <CarInfo label="Швидкість" value={speed} unit="км" icon={Gauge} className="text-white" />
+          <CarInfo label="Швидкість" value={speed} unit="км/г" icon={Gauge} className="text-white" />
           <CarInfo label="Заряд" value={battery.toFixed(1)} unit="%" icon={Battery} className="text-white" />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="space-y-4">
+            <h2>Car Systems</h2>
+            <SystemToggle name="Autopilot" active={systems.autopilot} onToggle={() => toggleSystems("autopilot")} />
+            <SystemToggle name="Climate-control" active={systems.climate} onToggle={() => toggleSystems("climate")} />
+            <SystemToggle name="Lights" active={systems.lights} onToggle={() => toggleSystems("lights")} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/SystemToggle.jsx
+++ src/components/SystemToggle.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { cn } from '../utils/cn';
+import { motion } from "framer-motion";
 
-const SystemToggle = () => {
+const SystemToggle = ({ name, active, onToggle }) => {
   return (
-    <div>
-      SystemToggle
-    </div>
+    <button onClick={onToggle} className={cn("flex items-center justify-between w-full p-4 rounded-xl border transition-all",
+      active ? "bg-blue-600/10 border-blue-500 text-blue-500" : "bg-zinc-900 border-zinc-800 text-zinc-500"
+    )}>
+      <span className='font-medium'>{name}</span>
+      <div className={cn("w-10 h-5 rounded-full relative transition-colors",
+        active ? "bg-blue-500" : "bg-zinc-700"
+      )}>
+        <motion.div animate={{ x: active ? 22 : 2 }} className='absolute top-1 left-0 w-3 h-3 bg-white rounded-full shadow-lg' />
+      </div>
+    </button>
   );
 }
 
